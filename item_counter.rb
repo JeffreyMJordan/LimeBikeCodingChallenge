c078fa54c39fcb5439eef7c7f69b0eed3f2ab615
JeffreyMJordan <jeff2016@alumni.stanford.edu>
2018-01-17 10:59:29 -0800

    Ensure that str is written to the terminal

--- item_counter.rb
+++ item_counter.rb
@@ -2,35 +2,6 @@ require 'date'
 require 'active_support/inflector'
 
 class ItemCounter 
-
-
-  #Takes in ride objects 
-    #Ride objects have start/end times and items list 
-  #Should be able to call print_items_per_interval()
-  #I assume print_items_per_interval() should take in increments of time 
-  #I think they're passing in the time objects like 07:00-07:10
-  
-
-
-  #Method 1: 
-  #Put all rides into an array and iterate through it
-  #If the (end_time is < interval_end_time && start_time>interval_start_time) or if (end_time>interval_end && start_time > interval_start), we count it
-  #This requires O(n) where n is the number of rides
-  #Also needs O(n) space complexity 
-  #Doesn't seem like there's a better way to do this than O(n)
-  #Can I do better? 
-    #Finding ideal start and end time doesn't seem like it would work (because everything could just be in range)
-  
-  #I guess I also need a way to keep track of items I need to return
-  #Ride could map item_str to number of items
-  #Then when I'm iterating, I create a master items hash, updating the total for each items_hash I find within range 
-  #Then I iterate through the master items hash and build the return_str 
-    #One concern - in their examples they returned the items in alphabetical order... 
-    #I'm going to assume that I don't need to sort it
-    #Or maybe I could do it for the printing, and have some other method called "find_items_in_interval" which returns the hash
-    #Yeah, I think I'll do that way, because then I can sort things
-  
-
   
   attr_reader :rides
 
@@ -46,10 +17,9 @@ class ItemCounter
     else 
       raise "Not a Ride object"
     end 
-    
   end   
 
-  #Time Complexity: O(nk) where n == @rides.length and k==ride.items.length
+  #Time Complexity: O(nk) where n == @rides.length and k == ride.items.length
   #Space Complexity: O(j) where j == total number of items in @rides 
   def find_items_in_interval(interval_start, interval_end)
     items_hash = Hash.new(0)
@@ -83,8 +53,8 @@ class ItemCounter
     end 
   end 
 
-  #Time Complexity: O(nk) because find_items_in_interval takes O(nk) (See above)
-  #Space Complexity: 
+  #Time Complexity: O(nk + q) where q is the size of hash returned by find_items_in_interval and O(nk) is the time complexity of find_items_in_interval  (See above)
+  #Space Complexity: O(j) where j is the size of hash (we must allocate slots in memory for our string proportional to the size of hash)
   def print_items_per_interval(interval_start, interval_end)
     hash = find_items_in_interval(interval_start, interval_end)
     str = ""
@@ -95,9 +65,9 @@ class ItemCounter
         str += "#{v} #{k}, "
       end 
     end
+    puts str[0..-3]  
     str[0..-3]  
   end 
-
 
 end 
 
